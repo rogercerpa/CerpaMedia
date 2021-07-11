4045321d1fee42f91c98d4c8a8971d795623bda0
ProRoger <roger.e.cerpa@gmail.com>
2021-07-11 00:42:24 -0400

    started the nextjs app for cerpamedia

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# CerpaMedia
+
+
+## Welcome to CerpaMedia
+
+we'll make sure your business stay's connected!
+
+```bash
+
+```
+
+Visit us here [CerpaMedia](http://www.cerpamedia.com) 
+
+## Learn More
+
+To learn more about our services and solutions, take a look at the following partners:
+
+- [KrasnovArt](https://www.krasnovart.com) - See all this beautiful resin art by Anastasiia!
+
+
+
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>CerpaMedia</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome, this site is under construction</h1>
+      <h2>
+      come back later.
+      </h2>
+      <h1>Thank you!</h1>
+    </div>
+  )
+}
