ae0fd1b8de9b3123ece83c1083d7473b507e90b2
rogercerpa <roger.e.cerpa@gmail.com>
2021-09-20 22:34:38 -0400

    updated the meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Theme>
         <Head>
-          <meta charset="UTF-8"/>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
          <title>CerpaMedia</title>
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
