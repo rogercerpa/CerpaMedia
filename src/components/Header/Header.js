7e8229119227e5fae651425fdf5c6c09fd2787d6
rogercerpa <roger.e.cerpa@gmail.com>
2023-02-05 23:27:43 -0500

    website updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,18 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import {TiMediaPlayOutline} from 'react-icons/ti'
+import Link from "next/link";
+import React from "react";
 
+import { TiMediaPlayOutline } from "react-icons/ti";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <Container>
-    <Div1>
-      <Link href="/">
-      <a style={{ display: "flex", alignItems: "center",  color: "white", marginBottom:"10px" }}>
-        <TiMediaPlayOutline size="6rem" /> <Span>CerpaMedia</Span>
-      </a>
-      </Link>
-    </Div1>
-        <Div3>
-
-      <SocialIcons >
-        <a href="https://github.com/rogercerpa" target="_blank" style={{ display: "flex", alignItems: "center", flexDirection: "column", color: "white" }} rel="noopener">
-          <AiFillGithub size="3rem" />
-        </a>
-      </SocialIcons>
-
-        <SocialIcons >
-        <a href="https://www.linkedin.com/in/roger-cerpa/" target="_blank" style={{ display: "flex", alignItems: "center", flexDirection: "column", color: "white" }} rel="noopener">
-          <AiFillLinkedin size="3rem" />
-        </a>
-      </SocialIcons>
-
-        <SocialIcons >
-        <a href="https://www.instagram.com/cerpamedia/" target="_blank" style={{ display: "flex", alignItems: "center", flexDirection: "column", color: "white" }} rel="noopener">
-          <AiFillInstagram size="3rem" />
-        </a>
-      </SocialIcons>
-
-
-
-    </Div3>
- </Container>
-);
+const Header = () => <Container></Container>;
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,48 +1,11 @@
-import React from 'react';
-import Link from 'next/link';
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span, Span1 } from './HeroStyles';
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import React from "react";
+
+import { Section, SectionTitle } from "../../styles/GlobalComponents";
 
 const Hero = (props) => (
   <Section>
     <SectionTitle>Welcome to CerpaMedia!</SectionTitle>
-    <SectionText>Our mission is to bring value and take solutions to the next level. </SectionText>
-
-          <Div2>
-      <li>
-        <Link href="/AboutUs">
-          <NavLink>
-            About Us
-          </NavLink>
-        </Link>
-      </li>
-       <li>
-        <Link href="/OurClients">
-          <NavLink>
-            Our Clients
-          </NavLink>
-        </Link>
-      </li>
-       <li>
-        <Link href="/Services">
-          <NavLink>
-            Services
-          </NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="/ContactUs">
-          <NavLink>
-            Contact Us
-          </NavLink>
-        </Link>
-      </li>
-    </Div2>
   </Section>
-  
-
 );
 
 export default Hero;
