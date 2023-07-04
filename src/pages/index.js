00838ce0a0850675982dc99cd42d74debf938b72
rogercerpa <roger.e.cerpa@gmail.com>
2023-07-03 22:56:25 -0400

    added developing comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
 import Layout from "../layout/Layout";
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const Home = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh", // or use 'calc(100vh - [HEIGHT_OF_YOUR_HEADER])' if you have a header
+        }}
+      >
+        <h1>Developing</h1>
+        <CircularProgress />
+      </Box>
+    </Layout>
+  );
 };
 
 export default Home;
